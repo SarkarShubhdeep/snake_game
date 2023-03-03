b73abc0f8e6e41d24e3b4eeca5d49a3fc10e0e99
Shubhdeep Sarkar <sarkarshubhdeep1@gmail.com>
2023-03-03 12:07:14 +0530

    Added directional movement

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 from turtle import Screen, Turtle
 import time
 
+from snake import Snake
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title('Snake')
 screen.tracer(0)
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+snake = Snake()
 
-for pos in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(pos)
-    segments.append(new_segment)
+sna = Turtle()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        x_cor = segments[seg_num-1].xcor()
-        y_cor = segments[seg_num-1].ycor()
-        segments[seg_num].goto(x_cor, y_cor)
+    snake.move()
 
-    segments[0].forward(20)
+
+
 
 
 
